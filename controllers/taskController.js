b96f34d0135d99d2eb09b3fcc6050877ce714afe
valentindush <codesvalentin@gmail.com>
2022-07-07 16:20:48 +0200

    added update task function

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,13 +1,14 @@
 const { taskSchema } = require("../models/task.model");
 const jwt = require("jsonwebtoken");
+const { model } = require("mongoose");
 module.exports.AddTask = async (req,res,next)=>{
     try {
 
-        const {token,type,description,deadline} = req.body;
+        const {token,type,description,deadline,id} = req.body;
 
         //Validations
 
-        if(!token || !type || !description || !deadline) return res.status(400).json({message: "all fields are required"});
+        if(!token || !type || !id || !description || !deadline) return res.status(400).json({message: "all fields are required"});
 
         //Verify token
 
@@ -26,12 +27,39 @@ module.exports.AddTask = async (req,res,next)=>{
 
         //Save task
         //Return task
-        
+
        if(await task.save()) res.status(201).json({message: "Task created successfully"});
          else res.status(500).json({message: "Error creating task"});
 
         
     } catch (err) {
         next(err);
+    }
+}
+
+module.exports.UpdateTask = async (req,res,next)=>{
+    try {
+
+        const {token,type,description,deadline,completed} = req.body;
+
+        //Validations
+
+        if(!token || !type || !description || !deadline || !completed) return res.status(400).json({message: "all fields are required"});
+        
+        //Verify token
+
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        if(!decoded) return res.status(401).json({message: "Invalid token"});
+
+        //update
+
+        const task = await taskSchema.findOneAndUpdate({_id: id}, {description,deadline,completed}, {new: true});
+        if(!task) return res.status(404).json({message: "Task not found"});
+        //Return task
+
+        return res.json({message: "Task updated successfully", status:true});
+
+    } catch (err) {
+        next(err)
     }
 }
--- routes/task.js
+++ routes/task.js
@@ -1,6 +1,7 @@
-const { AddTask } = require('../controllers/taskController');
+const { AddTask, UpdateTask } = require('../controllers/taskController');
 const router = require('express').Router();
 
 router.post('/addtask', AddTask)
+router.put('/updatetask', UpdateTask)
 
 module.exports.tasksRouter = router;
